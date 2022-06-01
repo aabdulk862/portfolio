9ffd48cf4affa566bcdc7504626e53cf57f60b3a
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-06-01 19:07:14 -0400

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/clothing.png'
-import IMG2 from '../../assets/smart-brain.png'
-import IMG3 from '../../assets/kanban.png'
-import IMG4 from '../../assets/nasa.png'
+import IMG2 from '../../assets/kanban.png'
+import IMG3 from '../../assets/nasa.png'
+import IMG4 from '../../assets/countdown.png'
 import IMG5 from '../../assets/quotegen.png'
-import IMG6 from '../../assets/quotegen.png'
+import IMG6 from '../../assets/joke-teller.png'
 
 // DO NOT USE THE IMAGES IN PRODUCTION
 
@@ -13,44 +13,44 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+    title: 'Royal Clothing Outlet - Made with React, Redux, GraphQL, Stripe, Firebase',
+    github: 'https://github.com/aabdulk862/royal-clothing-outlet',
+    demo: 'https://royal-clothing-outlet.netlify.app/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    title: 'Kanban Board - Project management tool designed to help visualize work',
+    github: 'https://github.com/aabdulk862/kanban-board',
+    demo: 'https://aabdulk862.github.io/kanban-board/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Figma dashboard UI kit for data design web apps',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+    title: 'NASA APOD - Uses the NASA API to get and save random astronomy pictures of the day',
+    github: 'https://github.com/aabdulk862/nasa-pictures-of-the-day',
+    demo: 'https://aabdulk862.github.io/nasa-pictures-of-the-day/'
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Maintaining tasks and tracking progress',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+    title: 'Custom Countdown - Counts down the days, hours, minutes, and seconds to a given date',
+    github: 'https://github.com/aabdulk862/custom-countdown-app',
+    demo: 'https://aabdulk862.github.io/custom-countdown-app/'
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    title: 'Quote Generator - Display a random quote from an api of quotes and tweet it to your followers',
+    github: 'https://github.com/aabdulk862/quote-generator',
+    demo: 'https://aabdulk862.github.io/quote-generator/'
   },
   {
     id: 6,
     image: IMG6,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    title: 'Joke Teller Robot -  Uses the VoiceRSS Text-to-speech API and the JokeAPI',
+    github: 'https://github.com/aabdulk862/joke-teller',
+    demo: 'https://aabdulk862.github.io/joke-teller/'
   }
 ]
 
