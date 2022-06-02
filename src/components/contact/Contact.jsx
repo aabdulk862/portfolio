621c125cea31d71a30cad321d063bb83aa8b609f
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-06-01 20:10:59 -0400

    contact stuff

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
@@ -12,7 +11,11 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_3fjfb17', 'template_ky1ucaz', form.current, 'user_641J0AWGxx4qcKi835yDq')
+    emailjs.sendForm('service_ysgxkhi', 'contact_form', form.current, 'k3JUJwgvZzI0bBR-E').then((result) => {
+      console.log(result.text);
+  }, (error) => {
+      console.log(error.text);
+  });
 
     e.target.reset()
   };
@@ -27,26 +30,20 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>dummyegator@gmail.com</h5>
-            <a href="mailto:dummyegator@gmail.com" target="_blank" rel="noreferrer" >Send a message</a>
-          </article>
-          <article className="contact__option">
-            <RiMessengerLine className='contact__option-icon'/>
-            <h4>Messenger</h4>
-            <h5>egatortutorials</h5>
-            <a href="https://m.me/ernest.achiever" target="_blank" rel="noreferrer" >Send a message</a>
+            <h5>adama1862@outlook.com</h5>
+            <a href="mailto:adama1862@outlook.com" target="_blank" rel="noreferrer" >Send a message</a>
           </article>
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone=+1234567890" target="_blank" rel="noreferrer" >Send a message</a>
+            <h5>+17033648616</h5>
+            <a href="https://wa.me/7033648616" target="_blank" rel="noreferrer" >Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="email" name='email' placeholder='Your Email' required />
+          <input type="text" name='user_name' placeholder='Your Full Name' required />
+          <input type="email" name='user_email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,15 +7,13 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/aabdulk862" className='footer__logo'>Adam Abdulkadir</a>
+      <a href="#header" className='footer__logo'>Adam Abdulkadir</a>
 
       <ul className='permalinks'>
         <li><a href="#header">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Expeirience</a></li>
-        <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
